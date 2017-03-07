662308b131feac4e5d9908bbfcd2290163715083
himanshu0113 <himanshuagarwal1395@gmail.com>
2017-03-07 17:01:35 +0530

    SURF working

--- CV_Project/SURF.h
+++ CV_Project/SURF.h
@@ -1,9 +1,14 @@
-#include "opencv2/core/core.hpp"
-#include "opencv2/features2d/features2d.hpp"
+#include "opencv2/core.hpp"
+#include "opencv2/core/utility.hpp"
+#include "opencv2/core/ocl.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/features2d.hpp"
+#include "opencv2/calib3d.hpp"
+#include "opencv2/imgproc.hpp"
 #include "opencv2/xfeatures2d.hpp"
-#include "opencv2/xfeatures2d/nonfree.hpp"
 
 /* @function callsurf
 * @brief SURF feature detector 
 */
-//void callsurf(Mat, Mat);
+void callsurf(Mat, Mat);
--- CV_Project/main.cpp
+++ CV_Project/main.cpp
@@ -5,12 +5,13 @@
 //#include "harris.h"
 //#include "fourier_transform.h"
 //#include "hist_chk.h"
-#include "tomasi.h"
+//#include "tomasi.h"
+#include "SURF.h";
 
 int main()
 {
 	Mat src = imread("cube1.jpg");
-	Mat src2 = imread("cube1.jpg");
+	Mat src2 = imread("cube2.jpg");
 	if (!src.data || !src2.data)
 	{
 		printf("Source image not found. Please check the source address.");
@@ -25,7 +26,8 @@ int main()
 	//callMH(src);
 	//callFourier(src);
 	//hist_chk(src);
-	calltomasi(src);
+	//calltomasi(src);
+	callsurf(src, src2);
 
 	/*Mat dst;
 	cvtColor(src, src, CV_BGR2GRAY);
